acc4b23aa2a4254818de582d6da45cfe49f0867b
Kervins Valcourt <kervins.valcourtz@gmail.com>
2017-01-17 18:29:55 -0500

    Initial Commit

---
+++ Grailed Code Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Grailed Code Challenge
+//
+//  Created by Kervins Valcourt on 1/17/17.
+//  Copyright © 2017 Grailed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
